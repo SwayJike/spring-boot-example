c952f674e8b4832f96fb2eb568fbc5cc5675f866
JourWon <jourwon@163.com>
2021-01-15 19:39:24 +0800

    first commit

---
+++ spring-boot-helloworld/src/main/java/com/jourwon/spring/boot/SpringBootHelloworldApplication.java
@@ -0,0 +1,19 @@
+package com.jourwon.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author JourWon
+ * @date 2021/1/15
+ */
+@SpringBootApplication
+public class SpringBootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworldApplication.class, args);
+	}
+
+}
