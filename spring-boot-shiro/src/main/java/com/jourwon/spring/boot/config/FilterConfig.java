389a3db51a126950a7f4cb989fc73110546e18e1
JourWon <JourWon@163.com>
2021-03-25 23:37:51 +0800

    bug:通过修改shiro依赖解决Consider defining a bean named 'authenticator' in your configuration.报错问题

---
+++ spring-boot-shiro/src/main/java/com/jourwon/spring/boot/config/FilterConfig.java
@@ -0,0 +1,29 @@
+package com.jourwon.spring.boot.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+/**
+ * Filter配置
+ *
+ * @author JourWon
+ * @date 2021/3/25
+ */
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean shiroFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+        //该值缺省为false，表示生命周期由SpringApplicationContext管理，设置为true则表示由ServletContainer管理
+        registration.addInitParameter("targetFilterLifecycle", "true");
+        registration.setEnabled(true);
+        registration.setOrder(Integer.MAX_VALUE - 1);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+
+}
