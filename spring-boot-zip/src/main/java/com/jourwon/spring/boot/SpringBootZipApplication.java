a816113bc30e5e820091ae6b106c0247993a3ef8
JourWon <JourWon@163.com>
2021-05-31 00:25:09 +0800

    feature:新增压缩和解压文件工具类

---
+++ spring-boot-zip/src/main/java/com/jourwon/spring/boot/SpringBootZipApplication.java
@@ -0,0 +1,19 @@
+package com.jourwon.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author JourWon
+ * @date 2021/5/30
+ */
+@SpringBootApplication
+public class SpringBootZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZipApplication.class, args);
+    }
+
+}
