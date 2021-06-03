3ad23621ecfc59cae18d4256b9bc7d50434618d6
JourWon <JourWon@163.com>
2021-06-04 00:35:22 +0800

    feature:完善Spring Boot整合Docker模块

--- spring-boot-docker/src/main/java/com/jourwon/spring/boot/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/jourwon/spring/boot/SpringBootDockerApplication.java
@@ -3,6 +3,12 @@ package com.jourwon.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author JourWon
+ * @date 2021/6/3
+ */
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
