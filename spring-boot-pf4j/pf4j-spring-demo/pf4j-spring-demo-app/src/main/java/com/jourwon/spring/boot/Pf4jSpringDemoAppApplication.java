5ec29b275f0f592d511773d6d83597c09f75568e
JourWon <JourWon@163.com>
2021-09-14 00:36:05 +0800

    feat:新增spring-boot-pf4j模块,Spring Boot整合pf4j,进行插件式编程拓展

---
+++ spring-boot-pf4j/pf4j-spring-demo/pf4j-spring-demo-app/src/main/java/com/jourwon/spring/boot/Pf4jSpringDemoAppApplication.java
@@ -0,0 +1,19 @@
+package com.jourwon.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author JourWon
+ * @date 2021/9/13
+ */
+@SpringBootApplication
+public class Pf4jSpringDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pf4jSpringDemoAppApplication.class, args);
+    }
+
+}
