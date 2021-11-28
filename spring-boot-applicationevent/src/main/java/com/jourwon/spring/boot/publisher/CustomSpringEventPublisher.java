bdb2d46486d9b8a52faa851bd74e9c5472d9c8e8
JourWon <JourWon@163.com>
2021-11-29 00:29:03 +0800

    feat:事件可以同时被多个监听者消费

--- spring-boot-applicationevent/src/main/java/com/jourwon/spring/boot/publisher/CustomSpringEventPublisher.java
+++ spring-boot-applicationevent/src/main/java/com/jourwon/spring/boot/publisher/CustomSpringEventPublisher.java
@@ -1,6 +1,7 @@
 package com.jourwon.spring.boot.publisher;
 
 import com.jourwon.spring.boot.event.CustomSpringEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
  * @author JourWon
  * @date 2021/11/27
  */
+@Slf4j
 @Component
 public class CustomSpringEventPublisher {
 
@@ -19,9 +21,11 @@ public class CustomSpringEventPublisher {
     ApplicationEventPublisher applicationEventPublisher;
 
     public void publishEvent(String message) {
+        log.info("开始发布自定义事件");
         CustomSpringEvent customSpringEvent = new CustomSpringEvent(this, message);
         // 发布事件
         applicationEventPublisher.publishEvent(customSpringEvent);
+        log.info("发布自定义事件结束");
     }
 
 }
