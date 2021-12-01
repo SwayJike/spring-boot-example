c14be3b0244cd32173c648c1f98f2d010e1bdc21
JourWon <JourWon@163.com>
2021-12-01 23:33:56 +0800

    feat:新增Spring Boot应用启动后执行任务项目spring-boot-runner

---
+++ spring-boot-runner/src/main/java/com/jourwon/spring/boot/SpringBootRunnerApplication.java
@@ -0,0 +1,19 @@
+package com.jourwon.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author JourWon
+ * @date 2021/12/1
+ */
+@SpringBootApplication
+public class SpringBootRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRunnerApplication.class, args);
+    }
+
+}
